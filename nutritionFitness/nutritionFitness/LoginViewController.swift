b36102bc8a6a657975ddb731437a62867fc7a4f8
Michael Fang <MikeFang456@gmail.com>
2021-11-12 09:14:48 -0800

    Added a segue to signup menu

--- nutritionFitness/nutritionFitness/LoginViewController.swift
+++ nutritionFitness/nutritionFitness/LoginViewController.swift
@@ -44,7 +44,7 @@ class LoginViewController: UIViewController {
         
         user.signUpInBackground{(success, error) in
             if success {
-                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                self.performSegue(withIdentifier: "signupSegue", sender: nil)
             } else {
                 print("Error: \(String(describing: error?.localizedDescription))")
             }
