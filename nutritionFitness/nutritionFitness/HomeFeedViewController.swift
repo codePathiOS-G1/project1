74f1a98120a67d52d7e591e6e33c7462e4f46db9
Michael Fang <MikeFang456@gmail.com>
2021-11-19 00:05:20 -0800

    Added Exercise, Meal, and Home feed storyboards

---
+++ nutritionFitness/nutritionFitness/HomeFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeFeedViewController.swift
+//  nutritionFitness
+//
+//  Created by Michael Fang on 11/18/21.
+//
+
+import UIKit
+
+class HomeFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
